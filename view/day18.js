80a988b091373c7300e81ae1d9aac3caed09d0ba
Dusan Zivkovic <dusan.zivkovic@cadev.rs>
2018-04-03 10:41:50 +0200

    project start

---
+++ view/day18.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {
+    Image,
+    StyleSheet,
+    LayoutAnimation,
+    Text,
+    TouchableHighlight,
+    PanResponder,
+    View
+} from 'react-native';
+
+
